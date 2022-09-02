c932061ab1e66c94a60bcf95ece3232b290ede2d
skys215 <skys215@gmail.com>
2022-09-02 15:40:47 +0800

    Fixed minor style bugs

--- materialize-stubs/auth-localized/login.blade.php
+++ materialize-stubs/auth-localized/login.blade.php
@@ -46,7 +46,7 @@
 
                 <br><br>
                 <div>
-                  <input class="btn right" type="submit" value="Log In">
+                  <input class="btn right" type="submit" value="{{ __('auth.sign_in') }}">
                   <a href="{{ route('register') }}" class="btn-flat">{{ __('auth.register') }}</a>
                 </div>
 
--- materialize-stubs/layouts/sidebar.blade.php
+++ materialize-stubs/layouts/sidebar.blade.php
@@ -1,4 +1,4 @@
 <ul id="sidenav-left" class="sidenav sidenav-fixed blue">
-  <li><a href="{{ route('home') }}" class="logo-container">{{ config('app.name') }}<i class="material-icons left">spa</i></a></li>
+  <li><a href="{{ route('home') }}" class="logo-container"><i class="material-icons">spa</i>{{ config('app.name') }}</a></li>
   @include('layouts.menu')
 </ul>
